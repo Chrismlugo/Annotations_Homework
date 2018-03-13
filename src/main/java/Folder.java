7b8f43d67d31d0e434beccf34011f7d08f8bdb92
Chris Murphy <_chrism_@live.com>
2018-03-13 17:03:46 +0000

    created folder and file classes

--- src/main/java/Folder.java
+++ src/main/java/Folder.java
@@ -1,2 +1,38 @@
+import java.util.Set;
+
 public class Folder {
+    private int id;
+    private String title;
+    private Set<File> files;
+
+    public Folder() {
+    }
+
+    public Folder(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(Set<File> files) {
+        this.files = files;
+    }
 }
